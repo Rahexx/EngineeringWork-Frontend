65d8c685e6cb34c08e116909c0e656219deb2e0d
Andrzej Łuszcz <endriu1297@gmail.com>
2020-10-17 16:48:32 +0200

    add event on burger to show nav

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './sass/main.scss';
+
+const burger = document.querySelector('.burger');
+const icon = document.querySelector('.fa-bars');
+const nav = document.querySelector('.navigation');
+
+burger.addEventListener('click', () => {
+
+    if(burger.classList.contains('active')){
+        icon.classList.remove('fa-times');
+        icon.classList.add('fa-bars');
+        burger.classList.remove('active');
+        nav.style.left = '-100vw';
+        return;
+    }
+
+    icon.classList.remove('fa-bars');
+    icon.classList.add('fa-times');
+    burger.classList.add('active');
+    nav.style.left = '0';
+})
