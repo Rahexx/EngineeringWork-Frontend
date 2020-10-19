2d406350a3bb62a660abeb8a571acd3fde570ffe
Andrzej Łuszcz <endriu1297@gmail.com>
2020-10-19 17:31:23 +0200

    change view logIn item in nav and add function to control nav

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
 import './sass/main.scss';
+import { gsap } from 'gsap';
 
 const burger = document.querySelector('.burger');
 const icon = document.querySelector('.fa-bars');
 const nav = document.querySelector('.navigation');
 
+const openNav = (width) => {
+  icon.classList.remove('fa-bars');
+  icon.classList.add('fa-times');
+  burger.classList.add('active');
+  gsap.to('.navigation', { x: width, duration: 1 });
+};
+
+const closeNav = () => {
+  icon.classList.remove('fa-times');
+  icon.classList.add('fa-bars');
+  burger.classList.remove('active');
+  nav.style.left = '-100vw';
+  gsap.to('.navigation', { x: 0, duration: 1 });
+};
+
 burger.addEventListener('click', () => {
+  const pageWidth = document.body.offsetWidth;
 
-    if(burger.classList.contains('active')){
-        icon.classList.remove('fa-times');
-        icon.classList.add('fa-bars');
-        burger.classList.remove('active');
-        nav.style.left = '-100vw';
-        return;
-    }
-
-    icon.classList.remove('fa-bars');
-    icon.classList.add('fa-times');
-    burger.classList.add('active');
-    nav.style.left = '0';
-})
+  if (burger.classList.contains('active')) {
+    closeNav();
+    return;
+  }
+  openNav(pageWidth);
+});
