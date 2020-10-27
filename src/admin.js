798747753ea575d3b8826baa05c2703175aa55cf
Andrzej Łuszcz <endriu1297@gmail.com>
2020-10-27 13:49:40 +0100

    add style for mobile horizontal

--- src/admin.js
+++ src/admin.js
@@ -1,9 +1,7 @@
 import Pagination from './app/Pagination';
 
-console.log('Wbiłem');
 const items = document.querySelectorAll('[data-value="download"]');
 const main = document.querySelector('.adminPanel__listUser');
-
 const pagination = new Pagination([...items]);
 
 pagination.createPagination(main);
