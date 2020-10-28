1dcaba54a8a29069bdc1f1b00bc6f1954f8ed0fc
Andrzej Łuszcz <endriu1297@gmail.com>
2020-10-28 16:21:39 +0100

    add popUp details

--- src/admin.js
+++ src/admin.js
@@ -1,7 +1,31 @@
 import Pagination from './app/Pagination';
+import { gsap } from 'gsap';
 
 const items = document.querySelectorAll('[data-value="download"]');
 const main = document.querySelector('.adminPanel__listUser');
+const detailsBtn = document.querySelectorAll('.adminPanel__button--details');
+const detailsInfo = document.querySelector('.detailsPopUp');
+const detailsBurger = document.querySelector('.burger--details')
 const pagination = new Pagination([...items]);
 
 pagination.createPagination(main);
+
+function closeDetails(){
+    detailsBurger.removeEventListener('click', closeDetails);
+    gsap.to('.detailsPopUp' , {left: '-100vw', duration: 1});
+    detailsBurger.style.display = 'none';
+}
+
+function showDetails(){
+    const pageWidth = document.body.offsetWidth;
+    detailsInfo.style.display = 'block';
+
+    gsap.to('.detailsPopUp' , {left: 0, duration: 1});
+
+    setTimeout(() => {
+        detailsBurger.style.display = 'flex';
+        detailsBurger.addEventListener('click', closeDetails);
+    }, 900);
+}
+
+[...detailsBtn].map((item) => item.addEventListener('click', showDetails));
