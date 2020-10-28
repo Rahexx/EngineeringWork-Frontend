8b95564791b1fdb2e3f2789394c7d6e19e14ecbc
Andrzej Łuszcz <endriu1297@gmail.com>
2020-10-28 18:13:51 +0100

    add new edit PopUp

--- src/admin.js
+++ src/admin.js
@@ -4,7 +4,7 @@ import { gsap } from 'gsap';
 const items = document.querySelectorAll('[data-value="download"]');
 const main = document.querySelector('.adminPanel__listUser');
 const detailsBtn = document.querySelectorAll('.adminPanel__button--details');
-const detailsInfo = document.querySelector('.detailsPopUp');
+const detailsInfo = document.querySelector('.popUp--details');
 const detailsBurger = document.querySelector('.burger--details')
 const pagination = new Pagination([...items]);
 
@@ -12,7 +12,7 @@ pagination.createPagination(main);
 
 function closeDetails(){
     detailsBurger.removeEventListener('click', closeDetails);
-    gsap.to('.detailsPopUp' , {left: '-100vw', duration: 1});
+    gsap.to('.popUp--details' , {left: '-100vw', duration: 1});
     detailsBurger.style.display = 'none';
 }
 
@@ -20,7 +20,7 @@ function showDetails(){
     const pageWidth = document.body.offsetWidth;
     detailsInfo.style.display = 'block';
 
-    gsap.to('.detailsPopUp' , {left: 0, duration: 1});
+    gsap.to('.popUp--details' , {left: 0, duration: 1});
 
     setTimeout(() => {
         detailsBurger.style.display = 'flex';
