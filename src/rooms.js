bd6093935848dc9cb12c5840aab884b742a3b36c
Andrzej Łuszcz <endriu1297@gmail.com>
2020-10-29 20:55:21 +0100

    add new feature, hears as add to favourite

--- src/rooms.js
+++ src/rooms.js
@@ -2,8 +2,25 @@ import Pagination from './app/Pagination';
 
 const items = document.querySelectorAll('[data-value="download"]');
 const main = document.querySelector('.searchRooms__list');
+const hearts = document.querySelectorAll('.searchRooms__heart');
 const pagination = new Pagination([...items], true);
 
-document.addEventListener('resize' , () => {
-    pagination.createPagination(main);
+function changeHeart(item) {
+  if (item.classList.contains('far')) {
+    item.classList.remove('far');
+    item.classList.add('fas');
+  } else {
+    item.classList.add('far');
+    item.classList.remove('fas');
+  }
+}
+
+document.addEventListener('resize', () => {
+  pagination.createPagination(main);
+});
+
+[...hearts].map((item) => {
+  item.addEventListener('click', () => {
+    changeHeart(item);
+  });
 });
