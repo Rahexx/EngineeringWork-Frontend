bfc20309cf1917047b4cd6323078d201251ad570
Andrzej Łuszcz <endriu1297@gmail.com>
2020-10-29 21:57:08 +0100

    add pagination to favourites page and add responsive styles for this page

--- src/favourite.js
+++ src/favourite.js
@@ -1,7 +1,7 @@
 import Pagination from './app/Pagination';
 
 const items = document.querySelectorAll('[data-value="download"]');
-const main = document.querySelector('.searchRooms__list');
+const main = document.querySelector('.favourites__list');
 const pagination = new Pagination([...items]);
 
 pagination.createPagination(main);
