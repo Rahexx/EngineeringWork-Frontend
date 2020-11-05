fbb7cf89f4b20ac9c51bf57b5b609aa9fe9e2d25
Andrzej Łuszcz <endriu1297@gmail.com>
2020-11-05 18:40:23 +0100

    create all style and animation for messages.html on mobile view

--- src/app/messages.js
+++ src/app/messages.js
@@ -1,1 +1,17 @@
 import '../sass/main.scss';
+import { gsap } from 'gsap';
+
+
+const users = document.querySelectorAll('.userList__item');
+const usersIcon = document.querySelector('.chat__users');
+
+[...users].map((item) => {
+    item.addEventListener('click', () => {
+        gsap.to('.chat', { x: 0, duration: 1 });
+    })
+})
+
+usersIcon.addEventListener('click', () => {
+        const width = document.body.offsetWidth;
+        gsap.to('.chat', { x: width, duration: 1 });
+    })
