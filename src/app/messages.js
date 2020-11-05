ca21ee0d2ad595285ad2d194a1a36980b5b56bd4
Andrzej Łuszcz <endriu1297@gmail.com>
2020-11-05 23:01:10 +0100

    add style for desktop

--- src/app/messages.js
+++ src/app/messages.js
@@ -7,11 +7,12 @@ const usersIcon = document.querySelector('.chat__users');
 
 [...users].map((item) => {
     item.addEventListener('click', () => {
-        gsap.to('.chat', { x: 0, duration: 1 });
+        const width = document.body.offsetWidth;
+        gsap.to('.chat', { x: width, duration: 1 });
     })
 })
 
 usersIcon.addEventListener('click', () => {
         const width = document.body.offsetWidth;
-        gsap.to('.chat', { x: width, duration: 1 });
+        gsap.to('.chat', { x: `-${width}`, duration: 1 });
     })
